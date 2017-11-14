0f39604dceba542a567e5a61e07582430d0442e7
Ryan Trauntvein <djdefi@github.com>
2017-11-13 22:58:22 -0800

    Shellcheck release helper

--- release-helper.sh
+++ release-helper.sh
@@ -13,7 +13,7 @@ set -e
 
 usage () {
     grep "^#/" <"$0" | cut -c4-
-    exit ${1:-2}
+    exit "${1:-2}"
 }
 
 [ $# -eq 0 ] && usage 1
@@ -42,8 +42,8 @@ delete_release () {
       exit 1
     fi
     echo "Removing release $cachet_version"
-    release_id=$(curl -H "Authorization: token $token" -s -X GET https://api.github.com/repos/CachetHQ/Docker/releases/tags/$cachet_version | jq -r .id)
-    curl -H "Authorization: token $token" -s -X DELETE https://api.github.com/repos/CachetHQ/Docker/releases/$release_id
+    release_id=$(curl -H "Authorization: token $token" -s -X GET https://api.github.com/repos/CachetHQ/Docker/releases/tags/"$cachet_version" | jq -r .id)
+    curl -H "Authorization: token $token" -s -X DELETE https://api.github.com/repos/CachetHQ/Docker/releases/"$release_id"
     git tag -d $cachet_version
     git push origin :$cachet_version
 }
@@ -62,7 +62,7 @@ ARGS=$(getopt --name "$0" --long help,delete,check --options hdc -- "$@") || {
   usage
   exit 2
 }
-eval set -- $ARGS
+eval set -- "$ARGS"
 
 while [ $# -gt 0 ]; do
   case "$1" in
@@ -88,7 +88,7 @@ while [ $# -gt 0 ]; do
   shift
 done
 
-if [ -z "cachet_version" ]; then
+if [ -z "$cachet_version" ]; then
     echo 1>&2 "error: no version specified."
     exit 1
 fi
@@ -96,19 +96,19 @@ fi
 #curl -H "Authorization: token $token" -s https://api.github.com/rate_limit
 
 # Make sure we are on clean branch
-git checkout -b cachet-$cachet_version
-    
+git checkout -b cachet-"$cachet_version"
+
 # Generate changelog (requires https://github.com/skywinder/github-changelog-generator)
 if hash github_changelog_generator 2>/dev/null; then
-  github_changelog_generator --token $token --future-release $cachet_version
+  github_changelog_generator --token "$token" --future-release "$cachet_version"
 fi
 
 # Modify Dockerfile, commit, tag, and push
 echo "Creating tag for $cachet_version"
-sed s/master/$cachet_version/g -i Dockerfile
+sed s/master/"$cachet_version"/g -i Dockerfile
 git commit -am "Cachet $cachet_version release"
-git tag -a $cachet_version -m "Cachet Release $cachet_version"
-git push origin $cachet_version
+git tag -a "$cachet_version" -m "Cachet Release $cachet_version"
+git push origin "$cachet_version"
 
 # Create GitHub release
-curl -H "Authorization: token $token" -s -H "Content-Type: application/json" -d '{"tag_name":"'${cachet_version}'","name":"'${cachet_version}'","body":"Cachet Release '${cachet_version}'","draft":false,"prerelease":false}' -X POST https://api.github.com/repos/CachetHQ/Docker/releases
+curl -H "Authorization: token $token" -s -H "Content-Type: application/json" -d '{"tag_name":"'"${cachet_version}"'","name":"'"${cachet_version}"'","body":"Cachet Release '"${cachet_version}"'","draft":false,"prerelease":false}' -X POST https://api.github.com/repos/CachetHQ/Docker/releases
